ee218becf9a7fbc242c4881665bcad9d265c30cf
Julio Echeverri <julio.echeverri@wolox.co>
2020-11-13 11:36:06 -0500

    Passed the second test successfully, now the delay function does not wait for intervals greater than 1000ms

--- finger-exercises/ecmascript/02-promises/index.js
+++ finger-exercises/ecmascript/02-promises/index.js
@@ -1,7 +1,11 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
+// This function doesn't await if the interval is greater than 1000ms
 export function delay(time) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    if (time > 1000) {
+      reject(new Error('This time is too much !'));
+    }
     const startTime = Date.now();
     setTimeout(() => {
       const elapsedTime = Date.now() - startTime;
