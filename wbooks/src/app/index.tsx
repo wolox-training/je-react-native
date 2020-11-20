21d01ec2b30cb11850ae80ffaa29a4d6432d15f8
Julio Echeverri <julio.echeverri@wolox.co>
2020-11-20 14:50:34 -0500

    Added new assets for the preview of a book that has no cover available

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import BookCard from '@components/BookCard';
 import { IBook } from '@interfaces/BookInfo';
 import { BOOKS_MOCK } from '@constants/mockBooks';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  }
+});
 
 const App = () => {
   const book: IBook[] = BOOKS_MOCK;
@@ -10,7 +16,7 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View>
+        <View style={styles.container}>
           {book.map(item => (
             <BookCard key={item.id} book={item} />
           ))}
