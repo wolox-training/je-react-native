ede3ec0ccc7e5567d718d1ea67dcf7271cb1c5be
Julio Echeverri <julio.echeverri@wolox.co>
2020-11-20 18:36:18 -0500

    Change style props for layout and shadow

--- wbooks/src/app/components/BookCard/styles.ts
+++ wbooks/src/app/components/BookCard/styles.ts
@@ -1,14 +1,23 @@
 import { StyleSheet } from 'react-native';
-import { DARK_GREY, WITHE_BACKGROUD } from '@constants/colors';
+import { DARK_GREY, WITHE_BACKGROUD, SHADOW_COLOR } from '@constants/colors';
 
 export default StyleSheet.create({
   container: {
-    width: '90%',
+    width: '100%',
     backgroundColor: WITHE_BACKGROUD,
-    flexShrink: 1,
     flexDirection: 'row',
     paddingVertical: 15,
-    paddingHorizontal: 30
+    paddingHorizontal: 30,
+    marginBottom: 10,
+    borderRadius: 5,
+    elevation: 1,
+    shadowColor: SHADOW_COLOR,
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    shadowOffset: {
+      height: 1,
+      width: 0
+    }
   },
   summaryContainer: {
     marginLeft: 20,
