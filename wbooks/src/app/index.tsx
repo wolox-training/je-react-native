50c8daf066a9b95da5e14a6d6525eb3c8fa948f9
Julio Echeverri <julio.echeverri@wolox.co>
2020-11-24 14:37:23 -0500

    Refactoring. Created the componente Libreary to encapsulate the BookList

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, View } from 'react-native';
-import BookList from '@components/BookList';
-import { IBook } from '@interfaces/BookInfo';
-import { BOOKS_MOCK } from '@constants/mockBooks';
-
-import styles from './styles';
+import { StatusBar } from 'react-native';
+import Library from '@screens/Library';
 
 const App = () => {
-  const book: IBook[] = BOOKS_MOCK;
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.container}>
-          <BookList bookArray={book} />
-        </View>
-      </SafeAreaView>
+      <Library />
     </>
   );
 };
