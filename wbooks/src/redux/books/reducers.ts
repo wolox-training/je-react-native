c2bf200989369f64a9cc75a41b69e98e9443fa2f
Julio Echeverri <julio.echeverri@wolox.co>
2020-11-26 07:53:15 -0500

    Refactor the reducer and added an initial state object

--- wbooks/src/redux/books/reducers.ts
+++ wbooks/src/redux/books/reducers.ts
@@ -1,8 +1,16 @@
 import { BOOKS_MOCK } from '@constants/mockBooks';
+import { actionNames as bookActionNames } from '@redux/books/actions';
+import { AppState } from '@interfaces/AppInfo';
+import { IBookAction } from '@interfaces/BookInfo';
 
-const reducerBooks = (state = [], action: any) => {
+const initialState: AppState = {
+  bookList: [],
+  idSelectedBook: null
+};
+
+const reducerBooks = (state = initialState, action: IBookAction): AppState => {
   switch (action.type) {
-    case 'GET_BOOK_LIST': {
+    case bookActionNames.getBookList: {
       return {
         ...state,
         bookList: BOOKS_MOCK
